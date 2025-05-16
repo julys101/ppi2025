1ace9de8ad19ac5e6acec6659db723635fc23396
julys101 <yjulyane@gmail.com>
2025-05-16 19:01:31 +0000

    Aula 16/05/25 - CSS: Criação de glogal e theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import './App.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Meu primeiro React App</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. 
+      </p>
     </div>
   );
 }
-
-export default App;
