f1999a3f4a43f1f6b446219639b8fb8947cd82d1
julys101 <yjulyane@gmail.com>
2025-06-26 18:43:38 +0000

    Aula - 26/06 Solução de desafio

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -4,7 +4,9 @@ export function MyHeader() {
   return (
     <header className={styles.container}>
       <div className={styles.fraseContainer}>
-        <h1 className={styles.title}>Foco, Força, Fé</h1>
+        <h1 className={styles.frase}>
+          <span className={styles.emoji}>🎲</span> Lucky Number Generator
+        </h1>
       </div>
     </header>
   );
