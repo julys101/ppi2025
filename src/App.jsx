2ec75d0994f3244ca3a88ff3d00a5250107862b2
julys101 <yjulyane@gmail.com>
2025-07-04 19:00:50 +0000

    Aula 04/07 - useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./styles/global.css";
 import "./styles/theme.css";
 import { MyHeader } from "./components/MyHeader";
 import { LuckyNumber } from "./components/LuckyNumber";
+import { ProductList } from "./components/ProductList";
 
 export default function App() {
   
   return (
      <>
-      <MyHeader/>
-      <LuckyNumber />
+      <ProductList/>
     </>
   );
 }
